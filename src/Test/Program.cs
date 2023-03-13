35e983fb9575f0b6c81a61eb0b1037d5aa7eabc8
Joel Christner <joel.christner@gmail.com>
2023-03-13 11:58:41 -0700

    NuGet v2.0.0, move to name value collection

---
+++ src/Test/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using GetSomeInput;
+using UrlMatcher;
+
+namespace Test
+{
+    class Program
+    {
+        static Matcher _Matcher = new Matcher();
+
+        static void Main(string[] args)
+        {
+            _Matcher.Logger = Console.WriteLine;
+
+            while (true)
+            {
+                string pattern = Inputty.GetString("Pattern :", "/{version}/foo/{whatever}/bar", false);
+                string url =     Inputty.GetString("URL     :", "/v1.0/foo/foobar/bar", false);
+
+                NameValueCollection vals = null;
+                bool success = _Matcher.Match(url, pattern, out vals);
+                if (success)
+                {
+                    Console.WriteLine("Match");
+                    if (vals != null && vals.Count > 0)
+                    {
+                        for (int i = 0; i < vals.Count; i++)
+                        {
+                            Console.WriteLine("  " + vals.GetKey(i) + ": " + vals.Get(i));
+                        }
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("No match");
+                }
+            }
+        }
+    }
+}
